e7136cc0ae4d2fdaf474d28bce214a5d2c94861c
anjwsm <anjalikushwahawsm@gmail.com>
2024-04-17 23:37:44 +0530

    first commit

---
+++ src/Pages/Video/Video.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Video.css'
+import { PlayVideo } from '../../Components/PlayVideo/PlayVideo';
+import { Recommended } from '../../Components/Recommended/Recommended';
+
+export const Video = () => {
+  return (
+    <div className='play-container'>
+     <PlayVideo/>
+     <Recommended/>
+
+    </div>
+  )
+}
+
+export default Video ;
