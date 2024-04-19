910cf91801c3d4caa7542741af6595aca3eedbcc
anjwsm <anjalikushwahawsm@gmail.com>
2024-04-20 00:37:57 +0530

    "Video Section"

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import './Home.css'
 import { Sidebar } from '../../Components/Sidebar/Sidebar'
 import { Feed } from '../../Components/Feed/Feed'
--- src/data.js
+++ src/data.js
@@ -1,1 +1,13 @@
 export const  API_KEY = 'AIzaSyBMMi74R58SVeFNav6dZOoYRxPOhijCqGQ' ;
+
+export const value_converter = (value) => {
+    if(value>=1000000){
+        return Math.floor(value/1000000) + "M"
+    }
+    else if(value>=1000){
+        return Math.floor(value/1000) + "K"
+    }
+    else {
+        return value ;
+    }
+}
